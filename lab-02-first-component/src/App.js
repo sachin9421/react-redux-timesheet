4b30ab25f05a46147c2090184d15ae706d99b0d2
z0033qj <Target9421>
2018-08-08 11:04:49 -0500

    extra credit

--- lab-02-first-component/src/App.js
+++ lab-02-first-component/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import Hello from '../src/hello/Hello'
+
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return <Hello />;
   }
 }
 
